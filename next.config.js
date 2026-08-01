0465a6441b644729c167c730555330cb607207cd
Neil Francis Teresa <neilfrancisteresa22@gmail.com>
2026-08-02 07:19:44 +0800

    Chnged next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-// Set this to your repo name if deploying to https://<user>.github.io/<repo>/
-// Leave as '' if this repo IS your username.github.io user/org page.
-const repoName = process.env.REPO_NAME || 'nft1025.github.io';
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-const basePath = isGithubActions && repoName ? `/${repoName}` : '';
-
 const nextConfig = {
-  output: 'export',
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined,
+  output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
